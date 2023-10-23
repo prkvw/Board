488593e892d085f01e3c02f32232acecc476f791
prkvw <neeqash@gmail.com>
2023-10-23 16:21:02 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom' ;
+import {FiSettings} from 'react-icons/fi';
+import {TooltipComponent} from '@syncfusion/ej2-react-popups';
 
-function App() {
+import './App.css'
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+    <div className="flex relative dark:bg-main-dark-bg">
+    <div className="fixed right-4 bottom-4" style={{ zIndex :'1000'}}>
+      <TooltipComponent content="Settings" position="Top">
+        <button type="button"
+        className ="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+        style={{ background: 'blue', borderRadius: '50%' }}>
+          <FiSettings className="h-6 w-6 text-main-dark-bg" />
+        </button>
+      </TooltipComponent>
     </div>
-  );
+    {activeMenu ?(
+    <div className="w-72 fixed sidebar
+    dark:bg-secondary-dark-bg bg-white">
+      Sidebar w-0
+      </div>) : (<div>
+      Sidebar w-0
+      </div>
+      )}
+      <div className={
+        `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ?
+       'md:ml-72' |: 'flex-2'}`}>
+         <div className=fixed md:static bg-main-main-bg dark:bg-main-dark-bg navbar w-full>
+          Navbar
+         </div>
+         <Routes>
+          {/* Dashboard */}
+          <Route path="/" element = "Ecommerce" />
+          <Route path="/ecommerce" element = "Ecommerce" />
+          {/* {Pages} */}
+          <Route path="/orders" element = "Orders" />
+          <Route path="/products" element = "Products" />
+          <Route path="/customers" element = "Customers" />
+          {/* Apps */}
+          <Route path="/apps" element = "Apps" />
+          <Route path="/apps/ecommerce" element = "Ecommerce" />
+          <Route path="/apps/ecommerce/orders" element = "Orders" />
+          <Route path="/apps/ecommerce/products" element = "Products" />
+          <Route path="/apps/ecommerce/customers" element = "Customers" />
+          {/* Charts */}
+          <Route path="/line" element = "Line" />
+          <Route path="/area" element = "Area" />
+          <Route path="/bar/" element = "Bar" />
+          <Route path="/pie/" element = "Pie" />
+          <Route path="/stacked/" element = "Stacked" />
+
+         </Routes>
+      </div>
+    </div>
+    </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
